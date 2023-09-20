ab017f3e7b6c4514cbf077498389f4ed0034ad8c
Red Hat <sonjuhy@gmail.com>
2023-09-21 01:13:35 +0900

    ✨ ADD Spring Boot Project
    - Add Spring Boot 3.1.3

---
+++ SpringBoot3/src/main/java/com/study/CSStudy/CsStudyApplication.java
@@ -0,0 +1,13 @@
+package com.study.CSStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsStudyApplication.class, args);
+	}
+
+}
---
+++ SpringBoot3/src/test/java/com/study/CSStudy/CsStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.CSStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
