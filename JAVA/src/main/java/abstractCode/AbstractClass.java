f90a7b101c136e7e52fa2274fda91fd0bd0b3590
Red Hat <sonjuhy@gmail.com>
2023-10-10 03:51:00 +0900

    ✨ ADD abstract class sample code
    - Add abstract class sample code

---
+++ JAVA/src/main/java/abstractCode/AbstractClass.java
@@ -0,0 +1,10 @@
+package abstractCode;
+
+public abstract class AbstractClass {
+    public void publicFunction(){}
+    protected void protectedFunction(){}
+    private void privateFunction(){}
+    public void methodInVal(){
+        int val = 0;
+    }
+}
---
+++ JAVA/src/main/java/abstractCode/AbstractClassImpl.java
@@ -0,0 +1,18 @@
+package abstractCode;
+
+public class AbstractClassImpl extends AbstractClass{
+    @Override
+    public void publicFunction() {
+        super.publicFunction();
+    }
+
+    @Override
+    protected void protectedFunction() {
+        super.protectedFunction();
+    }
+
+    @Override
+    public void methodInVal() {
+        super.methodInVal();
+    }
+}
