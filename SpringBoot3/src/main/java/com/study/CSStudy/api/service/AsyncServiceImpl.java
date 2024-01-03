4369faa620b5706d8d8a56b3a25a3dfad6be544d
Red Hat <sonjuhy@gmail.com>
2024-01-03 23:46:02 +0900

    ✨ Update Async feature
    - Update Async feature

---
+++ SpringBoot3/src/main/java/com/study/CSStudy/api/service/AsyncServiceImpl.java
@@ -0,0 +1,35 @@
+package com.study.CSStudy.api.service;
+
+import com.study.CSStudy.api.dto.FileDto;
+import com.study.CSStudy.db.repository.LargeDataCustomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
+import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+
+@Service
+public class AsyncServiceImpl {
+    @Autowired
+    LargeDataCustomRepository customRepository;
+    @Async("threadPoolTaskExecutor")
+    public CompletableFuture<Integer> saveWithBatchUpdateAsync(int id, List<FileDto> fileList){
+//        for(int i=0;i<5;i++){
+//            try {
+//                System.out.println(id+" async task start");
+//                Thread.sleep(3000);
+//                System.out.println(id+" async task end");
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
+        System.out.println(id+" is started!");
+        customRepository.saveAll(fileList);
+
+        return CompletableFuture.completedFuture(id);
+    }
+}
