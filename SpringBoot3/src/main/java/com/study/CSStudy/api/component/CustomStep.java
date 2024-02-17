bdd3c1209fd336b0a8e72c540d2d99fe544bea11
Red Hat <sonjuhy@gmail.com>
2024-02-17 17:01:27 +0900

    ✨ ADD custom step
    - ADD custom step

---
+++ SpringBoot3/src/main/java/com/study/CSStudy/api/component/CustomStep.java
@@ -0,0 +1,30 @@
+package com.study.CSStudy.api.component;
+
+import org.springframework.batch.core.JobInterruptedException;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class CustomStep implements Step {
+    private final static String stepName = "CustomStep";
+    @Override
+    public String getName() {
+        return stepName;
+    }
+
+    @Override
+    public boolean isAllowStartIfComplete() {
+//        return Step.super.isAllowStartIfComplete();
+        return true;
+    }
+
+    @Override
+    public void execute(StepExecution stepExecution) throws JobInterruptedException {
+        System.out.println("CustomStep execute");
+        List<Integer> list = (List<Integer>) stepExecution.getJobExecution().getExecutionContext().get("listData");
+        System.out.println(list);
+    }
+}
